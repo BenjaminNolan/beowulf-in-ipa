b3d248d5cbc040eb7262e560256d79f1a74ec147
Benjamin Nolan <ben.nolan@gmail.com>
2014-09-21 07:46:09 +0100

    God, I hate XML

--- src/formats/Html.php
+++ src/formats/Html.php
@@ -6,10 +6,10 @@ class Html
     public function build($lines)
     {
         $output = '<!DOCTYPE html><head><title>Beowulf in IPA</title></head><body><h1>Beowulf in IPA</h1><p>The following is the original text of the Old English epic poem Beowulf in IPA format.</p>';
-        $line = 1;
+        $lineNum = 1;
         foreach ($lines->p as $i => $p) {
             // Loop through each 'paragraph'
-            $output .= '<h2>Lines ' . $line . ' to ' . ($line + count($p->s)) . '</h2><p>';
+            $output .= '<h2>Lines ' . $lineNum . ' to ' . ($lineNum + count($p->s)) . '</h2><p>';
             foreach ($p->s as $j => $s) {
                 // Loop through each 'sentence'
                 if ($j > 0) $output .= '<br />';
@@ -20,7 +20,7 @@ class Html
                 die();
             }
             $output .= '</p>';
-            $line += count($p->s);
+            $lineNum += count($p->s);
         }
         $output .= '</body></html>';
 
--- src/formats/MacSay.php
+++ src/formats/MacSay.php
@@ -6,15 +6,20 @@ class MacSay
     public function build($lines)
     {
         $output = '';
-        $line = 1;
+        $lineNum = 1;
         foreach ($lines->p as $i => $p) {
             // Loop through each 'paragraph'
-            $output .= "\n# Lines {$line} to " . ($line + count($p->s)) . "\n";
+            $output .= "\n# Lines {$lineNum} to " . ($lineNum + count($p->s)) . "\n";
             foreach ($p->s as $j => $s) {
                 // Loop through each 'sentence'
+                $line = '';
+                foreach ($s as $m) {
+                    var_dump($m);
+                }
+                die();
                 $output .= "say --rate=120 -v Fiona \"/{$line}/\"\n";
             }
-            $line += count($p->s);
+            $lineNum += count($p->s);
         }
 
         file_put_contents(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'build' . DIRECTORY_SEPARATOR . 'mac-say-beowulf.sh', $output);
